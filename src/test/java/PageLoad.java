fafa522bc149f9edd1cfd6c8c7ef1c6796714fba
Ishita <ishita29.rai@gmail.com>
2024-09-18 20:17:11 +0530

    SELENIUM PRACTICE

---
+++ src/test/java/PageLoad.java
@@ -0,0 +1,8 @@
+import io.opentelemetry.sdk.common.export.ProxyOptions;
+
+public class Proxy {
+    public static void main(String[] args) {
+        Proxy proxy =new Proxy();
+        proxy.setHttp
+    }
+}
